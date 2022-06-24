a58d5cf972a09732dadc2abd113079cba2d35460
DiogolimalucasDev <diogolimalucas.dev@gmail.com>
2022-06-24 18:58:51 -0300

    tuplas e dicionarios

---
+++ aula61-tuplas/aula61.py
@@ -0,0 +1,10 @@
+# a diferença de tupla é que em tupla nao podemos alterar o valor da tupla
+
+tupla = (1,2,3, 'Diogo Lima')
+
+print(type(tupla))
+print(tupla[3])
+
+tupla1 = 1,2,3,4,5,6
+tupla2 = 7,8,9,10,11
+tupla3 = tupla1+ tupla2
---
+++ aula62-Diiconario/aula62.py
@@ -0,0 +1,23 @@
+d1 = {'chave1': 'valor1'}
+d1['nova chave'] = 'Valor da nova chave'
+
+print(d1)
+
+print(d1['chave1'])
+
+d2 = {
+        'str': 'valor' ,
+        123: 'Outro valor' ,
+        (12 , 34 , 5): 'Tupla'
+
+}
+
+print(d2[12 , 34 , 5])  # acessando a chave que esta contida por um tupla
+
+#outra mandeira de buscar a chave
+d2['nomedachave'] = 'Agora ela existe'
+if d2.get('nomedachave') is not None:
+    print(d2.get('nomedachave'))
+else:
+    print("nao existe")
+
