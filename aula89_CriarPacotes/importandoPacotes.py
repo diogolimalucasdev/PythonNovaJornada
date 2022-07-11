9a2faa5c636f0cdc203dfc41bcf2345ab804ca57
DiogolimalucasDev <diogolimalucas.dev@gmail.com>
2022-07-11 17:47:53 -0300

    Importando modulos e criando pacotes

--- aula89_CriarPacotes/importandoPacotes.py
+++ aula89_CriarPacotes/importandoPacotes.py
@@ -1,6 +1,5 @@
 # primeira maneira
 import vendas.calc_precos
-
 preco = 49.90
 # acesso a pasta vendas, dentro de vendas acesso ao arquivo calc_precos e dentro dele a função aumento
 preco_com_aumento = vendas.calc_precos.aumento(valor = preco , porcentagem = 15)
@@ -14,9 +13,12 @@ print(preco_com_aumento2)
 
 # terceira forma
 from aula89_CriarPacotes.vendas.calc_precos import reducao
-
 preco3 = 44.90
 preco_com_reducao = reducao(valor = preco3 , porcentagem = 10, formata=True)
 print(preco_com_reducao)
 
+
+#importando meu pacote
+from vendas.formata import preco
+print(preco.real(20))
 
