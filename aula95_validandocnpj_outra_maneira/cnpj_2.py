14c34df5cb33eca4d7e56f0d2c2b1c2ca36ed859
DiogolimalucasDev <diogolimalucas.dev@gmail.com>
2022-07-13 16:08:38 -0300

    validando cnpj de outra maneira

--- aula95_validandocnpj_outra_maneira/cnpj_2.py
+++ aula95_validandocnpj_outra_maneira/cnpj_2.py
@@ -6,8 +6,11 @@ REGRESSIVOS = [6 , 5 , 4 , 3 , 2 , 9 , 8 , 7 , 6 , 5 , 4 , 3 , 2]
 def valida(cnpj):
     cnpj = apenas_numeros(cnpj)
 
-    if eh_sequencia(cnpj):
-        print("É uma sequencia")
+    try:
+        if eh_sequencia(cnpj):
+            print("É uma sequencia")
+            return False
+    except:
         return False
 
     try:
